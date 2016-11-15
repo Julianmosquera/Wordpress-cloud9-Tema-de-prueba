ec56f6a9fe109da92a86c87da7609708c07bc059
Julián Mosquera <julianmosqueraguimerans@gmail.com>
2016-11-15 18:27:56 +0000

    single + page

---
+++ Julian-tema/404.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>No se enceunta la pagina</h1>
+</br>
+<IMG SRC="wp-content/themes/Julian-tema/Page-Not-Found-18.jpg">
--- Julian-tema/index.php
+++ Julian-tema/index.php
@@ -1,15 +1,23 @@
 
-<?php get_header( $TOP ); ?>
-<?php echo '<p>Hola Mundo</p>'; ?>
+<?php get_header(); ?>
+<?php echo '<p>Hola Mundo </p>'; ?>
 <?php 
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post(); 
-		//
-		// Post Content here
-		//
-	} // end while
-} //end if
-?>
 
-<?php get_footer( $Abajo); ?>
+if (have_posts()) :
+   while (have_posts()) :
+      the_post();
+      the_content();
+   endwhile;
+endif;
+echo "Listo"
+?>
+</br
+
+<h2 id="post-<?php the_ID(); ?>">
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+<?php the_title(); ?></a></h2>
+<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+
+
+
+<?php get_footer(); ?>
